6ae3a36f9a0f86cacad99d00da24e4bf5a5d6ce2
Emrullah AKAN <akanemrullah@gmail.com>
2024-02-21 21:34:50 +0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Degiskenler tanimlandi.
         int mesafe, yas, yolculukTipi;
         double mUcret = .10, total;
 
+        // Scanner sinifina ait elemani tanimladik.
         Scanner input = new Scanner(System.in);
 
+        // Kullanicidan yolculuk ile alakali bilgiler alinip bu bilgiler ilgili degiskenlere atandi.
         System.out.print("Mesafeyi kilometre cinsinden gir: ");
         mesafe = input.nextInt();
 
@@ -17,45 +20,39 @@ public class Main {
         System.out.print("Yolculuk tipini gir: ");
         yolculukTipi = input.nextInt();
 
+        // Burada indirimsiz haliyle ucusun toplam fiyatini hesapladik.
         total = mesafe * mUcret;
 
+        // Gerekli mantiksal kontroller ve kosullara uyan indirimler tanimlandi. Ciktimizi yine bu alanda hesapladik.
         if (mesafe >= 0 && yas >= 0) {
-
-            switch (yolculukTipi) {
-
-                case 1:
-                    if (yas < 12) {
-                        total = total - (total * .5);
-                    } else if (yas >= 12 && yas <= 24) {
-                        total = total - (total * .1);
-                    } else if (yas > 65) {
-                        total = total - (total * .3);
-                    }
-                    break;
-
-                case 2:
-                    if (yas < 12) {
-                        total -= total * .5;
-                        total -= total * .2;
-                        total *= 2;
-                    } else if (yas >= 12 && yas <= 24) {
-                        total -= total * .1;
-                        total -= total * .2;
-                        total *= 2;
-                    } else if (yas > 65) {
-                        total -= total * .3;
-                        total -= total * .2;
-                        total *= 2;
-                    }
-                    break;
-                default:
-                    System.out.println("Hatali deger girdin.");
-                    break;
+            if (yolculukTipi == 1) {
+                if (yas < 12) {
+                    total -= total * .5;
+                } else if (yas >= 12 && yas <= 24) {
+                    total -= total * .1;
+                } else if (yas > 65) {
+                    total -= total * .3;
+                }
+            } else if (yolculukTipi == 2) {
+                if (yas < 12) {
+                    total -= total * .5;
+                    total -= total * .2;
+                    total *= 2;
+                } else if (yas >= 12 && yas <= 24) {
+                    total -= total * .1;
+                    total -= total * .2;
+                    total *= 2;
+                } else if (yas > 65) {
+                    total -= total * .3;
+                    total -= total * .2;
+                    total *= 2;
+                }
+            } else {
+                System.out.println("Hatali deger girdin.");
             }
-            System.out.println("Tutar : " + total);
-
+            System.out.println("Tutar : " + total + " TL");
         } else {
-            System.out.println("Hatali deger girdin. Tekrar dene");
+            System.out.println("Hatali deger girdin.");
         }
     }
 }
